c6415bc3ad7862aad294a60b5912c9aadd908ffd
Norman Khine <khinester@aqoon.local>
2012-01-13 09:37:44 +0100

    fix region info and add fade

--- js/init.js
+++ js/init.js
@@ -40,7 +40,7 @@ function drawMap(paths) {
 		obj.attr(attributes);
 		arr[obj.id] = country;
 		
-		var countryinfo = $("<div id='"+obj.id+"' style='display:none;' class='country'><p style='font-size:15px;'>Country Name:<a href='#' class='link' >"+paths[country].name+"</a></p></div>");
+		var countryinfo = $("<div id='"+obj.id+"' style='display:none;' class='country' ><img src='img/close.png' class='closeinfo' style='float:right;' /><p style='font-size:15px;'>Country Name:<a href='#' class='link' >"+paths[country].name+"</a></p></div>");
 		var countryflag = $("<img src='flags/"+paths[country].name+".png' alt='There is no flag.' class='flag' />"); countryinfo.append(countryflag);
 		var totalmembers = $("<p>Total members:"+paths[country].total_members+"</p>"); countryinfo.append(totalmembers);
 		info.append(countryinfo);
@@ -61,11 +61,15 @@ function drawMap(paths) {
 			$("#countryinfo").css({"margin-left": e.pageX-10});
 			$("#countryinfo").css({"margin-top": e.pageY-10});
 			$("#countryinfo").css({"display": "block"});
-			document.getElementById(this.id).style.display = "block";
+			$("#"+this.id).css({"display": "block"});
 
 			var id = this.id; var name = arr[this.id];
 			//$("#" + this.id + " .flag").click( function(){ exapandCountry(id, name); });
 			
+			$(".closeinfo").click(function() { 
+				$("#countryinfo").css({"display":"none"});
+				$("#"+this.id).css({"display":"none"});			
+			});			
 		}, function(){
 			this.animate({
 				fill: attributes.fill
@@ -110,23 +114,31 @@ function drawCountry(data) {
 		arr[obj.id] = region;
 		
 		var region_area = document.getElementById(regions[region].shortName);
-		var regioninfo = $("<div id='"+regions[region].shortName+obj.id+"' style='display:none;'><p style='font-size:15px;'>Region Name:"+regions[region].name+"</p></div>");
+		var regioninfo = $("<div id='"+ regions[region].shortName+obj.id +"' style=' display:none; ' class=' region ' ><img src='img/close.png' class='closeinfo' style='float:right;' /><p style='font-size:15px;'>Region Name:<a href='#' class='link' >"+regions[region].name+"</a></p></div>");
 		var countryflag = $("<img src='flags/"+regions[region].country+".png' alt='There is no flag.' />"); regioninfo.append(countryflag);
 		var totalmembers = $("<p>Total members:"+regions[region].total_members+"</p>"); regioninfo.append(totalmembers);
 		regioninfo.appendTo(region_area);
 				
-		obj.hover(function(){
+		obj.hover(function(e){
+			$(".regioninfo").css({ "display": "none" });
+			$(".region").css({ "display": "none" });		
 			this.animate({
 				fill: '#1669AD'
 			}, 300);
-			document.getElementById(regions[region].shortName).style.display = "block";
-			document.getElementById(regions[region].shortName+this.id).style.display = "block";			
+			
+			$("#" + regions[region].shortName).css({"margin-left": e.pageX-10});
+			$("#" + regions[region].shortName).css({"margin-top": e.pageY-10});
+			$("#" + regions[region].shortName).css({"display":"block"});
+			$("#" + regions[region].shortName+this.id).css({"display":"block"});
+			
+			$(".closeinfo").click(function() { 
+				$("#" + regions[region].shortName).css({"display":"none"});
+				$("#" + regions[region].shortName+this.id).css({"display":"none"});			
+			});
 		}, function(){
 			this.animate({
 				fill: attributes.fill
 			}, 300);
-			document.getElementById(regions[region].shortName).style.display = "none";
-			document.getElementById(regions[region].shortName+this.id).style.display = "none";			
 		});
 		
 	}	
